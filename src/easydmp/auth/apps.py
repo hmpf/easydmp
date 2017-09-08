47a56f6eafd34f48fb7a23bd07a8689b6cccdafe
Hanne Moa <hanne.moa@uninett.no>
2017-09-08 09:38:35 +0200

    Initial site setup

---
+++ src/easydmp/auth/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class EasyDMPAuthConfig(AppConfig):
+    name = 'easydmp.auth'
+    verbose_name = _("EasyDMP Authentication and Authorization")
+    label = 'easydmp_auth'
---
+++ src/easydmp/site/settings/dev.py
@@ -0,0 +1,6 @@
+from .base import *
+
+DEBUG = True
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+SECRET_KEY = '=%rr$)2d&hl)#u0kgfgt**%-xmz!#r#1-#px-=nwu)j&2#a-2m'
