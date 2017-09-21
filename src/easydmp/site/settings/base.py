ab2eb6a93cfd9f0273b48307d0a6dcdfd6aee4fb
Hanne Moa <hanne.moa@uninett.no>
2017-09-21 10:55:42 +0200

    Autogenerate docs, accessible from admin

--- src/easydmp/site/settings/base.py
+++ src/easydmp/site/settings/base.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
