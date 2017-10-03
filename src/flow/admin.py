1c1bdfefba9120838e7f8ce9d0d973a4d82d5bea
Hanne Moa <hanne.moa@uninett.no>
2017-10-03 08:28:35 +0200

    Improve flow admin

--- src/flow/admin.py
+++ src/flow/admin.py
@@ -5,17 +5,27 @@ from .models import Edge
 from .models import FSA
 
 
+class PrevEdgeInline(admin.StackedInline):
+    model = Edge
+    fk_name = 'prev_node'
+
+
 class EdgeAdmin(admin.ModelAdmin):
     list_display = ['condition', 'prev_node', 'next_node']
 admin.site.register(Edge, EdgeAdmin)
+
+class NodeInline(admin.StackedInline):
+    model = Node
 
 
 class NodeAdmin(admin.ModelAdmin):
     list_display = ['slug', 'depends', 'fsa']
     list_filter = ['fsa']
+    inlines = [PrevEdgeInline]
 admin.site.register(Node, NodeAdmin)
 
 
 class FSAAdmin(admin.ModelAdmin):
-    list_display = ['id',]
+    list_display = ['slug', 'id']
+    inlines = [NodeInline]
 admin.site.register(FSA, FSAAdmin)
