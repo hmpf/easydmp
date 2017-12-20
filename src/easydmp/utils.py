01592f5ac44d85af78c7d8a035b94935d07ab9a0
Hanne Moa <hanne.moa@uninett.no>
2017-12-20 13:49:06 +0100

    Add function to get utc epoch

--- src/easydmp/utils.py
+++ src/easydmp/utils.py
@@ -1,4 +1,10 @@
-__all__ = ['pprint_list']
+from django.utils.timezone import now as utcnow
+
+
+__all__ = [
+    'pprint_list',
+    'iso_epoch',
+]
 
 
 def pprint_list(list_):
@@ -8,3 +14,9 @@ def pprint_list(list_):
         return list_[0]
     first_part = ', '.join(list_[:-1])
     return '{} and {}'.format(first_part, list_[-1])
+
+
+def utc_epoch(utc_datetime=None):
+    if not utc_datetime:
+        utc_datetime = utcnow()
+    return utc_datetime.timestamp()
