892d643b2d602f8f1ba27aa67a6c89adca81ec68
Hanne Moa <hanne.moa@uninett.no>
2018-01-22 13:11:01 +0100

    Make purge_answer() save the changes, even

--- src/easydmp/plan/utils.py
+++ src/easydmp/plan/utils.py
@@ -1,7 +1,11 @@
 def purge_answer(plan, question_pk):
     "Delete and return the answer for the specified plan and question id"
 
-    data = plan.data.pop(question_pk, None)
-    prevdata = plan.previous_data.pop(question_pk, None)
+    data = plan.data
+    data.pop(question_pk, None)
+    prevdata = plan.previous_data
+    prevdata.pop(question_pk, None)
+    plan.data = data
+    plan.previous_data = prevdata
     plan.save()
     return {'data': data, 'previous_data': prevdata}
