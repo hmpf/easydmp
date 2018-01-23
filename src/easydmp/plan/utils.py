7b9129bffb1b17a75a96477f9b581bae0d98f664
Hanne Moa <hanne.moa@uninett.no>
2018-01-23 14:09:25 +0100

    Add more utils for changing existing plans

--- src/easydmp/plan/utils.py
+++ src/easydmp/plan/utils.py
@@ -9,3 +9,21 @@ def purge_answer(plan, question_pk):
     plan.previous_data = prevdata
     plan.save()
     return {'data': data, 'previous_data': prevdata}
+
+
+
+def convert_ee_to_eenotlisted(choice):
+    if isinstance(choice, list):
+        choices = choice
+        not_listed = True
+        choice = {'choices': choices, 'not_listed': not_listed}
+        return choice
+    else:
+        return choice
+
+
+def convert_eenotlisted_to_ee(choice):
+    if isinstance(choice, dict):
+        return choice['choices']
+    else:
+        return choice
