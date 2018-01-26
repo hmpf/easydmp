a69028575fc25a7f72ca3cecdf768c5b0a5ea724
Hanne Moa <hanne.moa@uninett.no>
2018-01-26 07:56:02 +0100

    Adjust CSS as per meeting 2018-01-25
    .. and rename "Go to overview"

--- src/easydmp/theme/static/usit/usitoverride.js
+++ src/easydmp/theme/static/usit/usitoverride.js
@@ -1,11 +1,12 @@
 /*! jQuery v1.11.3 | (c) 2005, 2015 jQuery Foundation, Inc. | jquery.org/license */
 $( document ).ready(function() {
+
   //$(".uninett-color-darkBlue").css("background-color", "red");
-  $('h3').each(function() {
-         if ($(this).text() == "") {
+  /*$('h2').each(function() {
+         if ($(this).text() == "Existing plans") {
                    //Do Something
-                   $(this).css("border-top", "none");
                    $(this).css("display", "none");
          }
-       });
+       });*/
+      /* $( "<div class='divider'></div>" ).insertBefore( ".jumbotron" );*/
 });
