1375ac02d08c85d32d0c9570aae2aa4c6e6bc087
Hanne Moa <hanne.moa@uninett.no>
2018-02-12 14:24:17 +0100

    Filter plans on templates

--- src/easydmp/plan/admin.py
+++ src/easydmp/plan/admin.py
@@ -6,4 +6,5 @@ from .models import Plan
 @admin.register(Plan)
 class PlanAdmin(admin.ModelAdmin):
     list_display = ['title', 'template', 'added_by', 'added']
+    list_filter = ['template']
     readonly_fields = ['added', 'added_by']
