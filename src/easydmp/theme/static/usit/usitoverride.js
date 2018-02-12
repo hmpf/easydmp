998be8795e06adc81bacf54bed77bd9a6e631cfa
Hanne Moa <hanne.moa@uninett.no>
2018-02-12 11:51:31 +0100

    Update css/js as per mail from Iselin, 20180212

--- src/easydmp/theme/static/usit/usitoverride.js
+++ src/easydmp/theme/static/usit/usitoverride.js
@@ -1,12 +1,17 @@
 /*! jQuery v1.11.3 | (c) 2005, 2015 jQuery Foundation, Inc. | jquery.org/license */
 $( document ).ready(function() {
 
-  //$(".uninett-color-darkBlue").css("background-color", "red");
-  /*$('h2').each(function() {
-         if ($(this).text() == "Existing plans") {
-                   //Do Something
-                   $(this).css("display", "none");
-         }
-       });*/
-      /* $( "<div class='divider'></div>" ).insertBefore( ".jumbotron" );*/
+  if ( $( ".uninett-whole-row").hasClass("progressbar") ){
+    console.log("Denne siden har progressbar");
+    $(".section").css({"border-top": "none", "padding": "0", "margin": "0", "padding-top": "1.5em"});
+    $(".summary-link").css({"left": "0", "top": "-2.3em"});
+
+    /*var link = "";
+    $( ".progressbar ul li" ).each(function() {
+      link =  $( this ).find("a").attr("href");
+        console.log(link);
+        setlink = "<a href="+ link +"></a>";
+        $( ".progressbar ul li span" ).wrap( setlink );
+    });*/
+  }
 });
