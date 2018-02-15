fa6b1b94632680384070d07021d1ff0384c9fa90
Hanne Moa <hanne.moa@uninett.no>
2018-02-15 14:03:14 +0100

    Adjust to newest css/js from Iselin, 20180215

--- src/easydmp/theme/static/usit/usitoverride.js
+++ src/easydmp/theme/static/usit/usitoverride.js
@@ -13,77 +13,18 @@ $( document ).ready(function() {
       $(this,"span").wrap( setlink );
     });
   }
+  var actiondd = "<div class='actiondd'></div>"
+  $(".dmptable tbody tr td:last-child").wrap(actiondd);
 });
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('.navbar').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+$(window).bind('mousewheel', function(event) {
+    if ( (event.originalEvent.wheelDelta >= 0) && ($(document).scrollTop() < 100) ) {
+        $( ".progressbar").addClass("withfadeout");
+        $( ".progressbar" ).removeClass("progressbar-small");
     }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $( ".navbar-fixed-top" ).addClass("slider");
-        $( ".navbar-fixed-top" ).addClass("closed");
-        $( ".jumbotron" ).addClass("jumbotron-small");
-        $( ".progressbar").addClass("withfadein");
+    else {
         $( ".progressbar li span").addClass("withfadein");
-        $( ".progressbar").addClass("progressbar-small");
-      //  $( ".navbar-fixed-top" ).slideUp( "fast", function() {
-  // Animation complete.
-        //});
-      //  $('.navbar').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-          $( ".navbar-fixed-top" ).removeClass("closed");
-          $( ".progressbar").addClass("withfadeout");
-          $( ".progressbar" ).removeClass("progressbar-small");
-          $( ".jumbotron" ).removeClass("jumbotron-small");
-          //$( ".navbar-fixed-top" ).slideDown( "fast", function() {
-    // Animation complete.
-          //});
-            //$('.navbar').removeClass('nav-up').addClass('nav-down');
-        }
+        $( ".progressbar").removeClass("withfadeout");
+        $( ".progressbar" ).addClass("progressbar-small");
     }
-
-    lastScrollTop = st;
-}
-
-
-/*var iScrollPos = 0;
-$(window).scroll(function () {
-    var iCurScrollPos = $(this).scrollTop();
-    if (iCurScrollPos > iScrollPos) {
-        console.log("//Scrolling Down");
-        $(".navbar").css("display", "none");
-        $("body").css("padding-top", "0");
-        $(".progressbar").css("display", "none");
-    } else {
-       console.log("//Scrolling Up");
-       $(".navbar").css("display", "block");
-       $("body").css("padding-top", "70px");
-       $(".progressbar").css("display", "block");
-    }
-iScrollPos = iCurScrollPos;
-}); */
+});
