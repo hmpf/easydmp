dab43ba302aa14ad3aad74e385bfc2e3172dcfff
Hanne Moa <hanne.moa@uninett.no>
2018-02-23 10:09:20 +0100

    Strip spurious newlines in envvars

--- src/easydmp/site/settings/production.py
+++ src/easydmp/site/settings/production.py
@@ -4,16 +4,22 @@ import dj_database_url
 
 from .base import *
 
+def getenv(name, default=None):
+    value = os.getenv(name, default)
+    if isinstance(value, str):
+        env = value.strip()
+    return value
+
 STATIC_ROOT = 'staticfiles'
 # Changeable settings
-EMAIL_HOST = os.getenv('EMAIL_HOST', None)
+EMAIL_HOST = getenv('EMAIL_HOST', None)
 assert EMAIL_HOST, 'Env "EMAIL_HOST" not set'
-EMAIL_PORT = os.getenv('EMAIL_PORT', 25)
+EMAIL_PORT = getenv('EMAIL_PORT', 25)
 
-SECRET_KEY = os.getenv('SECRET_KEY', None)
+SECRET_KEY = getenv('SECRET_KEY', None)
 assert SECRET_KEY, 'Env "SECRET_KEY" not set'
 
-DATABASE_URL = os.getenv('DMP_DATABASE_URL', None)
+DATABASE_URL = getenv('DMP_DATABASE_URL', None)
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.parse(DATABASE_URL),
@@ -25,7 +31,7 @@ except NameError:
     # Assure that DEBUG is set and safe
     DEBUG = False
 
-DEBUG = bool(int(os.getenv('DEBUG', DEBUG)))
+DEBUG = bool(int(getenv('DEBUG', DEBUG)))
 
 INSTALLED_APPS += [
     'social_django',
@@ -39,6 +45,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+LOGGING_CONFIG = None
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -62,15 +69,18 @@ LOGGING = {
         },
     },
 }
+import logging.config
+logging.config.dictConfig(LOGGING)
+
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 # Other settings
 
-EASYDMP_INVITATION_FROM_ADDRESS = os.getenv('EASYDMP_INVITATION_FROM_ADDRESS', None)
+EASYDMP_INVITATION_FROM_ADDRESS = getenv('EASYDMP_INVITATION_FROM_ADDRESS', None)
 assert EASYDMP_INVITATION_FROM_ADDRESS, 'Env "EASYDMP_INVITATION_FROM_ADDRESS" not set'
 
-SOCIAL_AUTH_DATAPORTEN_EMAIL_KEY = os.getenv('SOCIAL_AUTH_DATAPORTEN_EMAIL_KEY')
-SOCIAL_AUTH_DATAPORTEN_EMAIL_SECRET = os.getenv('SOCIAL_AUTH_DATAPORTEN_EMAIL_SECRET')
-SOCIAL_AUTH_B2ACCESS_KEY = os.getenv('SOCIAL_AUTH_B2ACCESS_KEY')
-SOCIAL_AUTH_B2ACCESS_SECRET = os.getenv('SOCIAL_AUTH_B2ACCESS_SECRET')
+SOCIAL_AUTH_DATAPORTEN_EMAIL_KEY = getenv('SOCIAL_AUTH_DATAPORTEN_EMAIL_KEY')
+SOCIAL_AUTH_DATAPORTEN_EMAIL_SECRET = getenv('SOCIAL_AUTH_DATAPORTEN_EMAIL_SECRET')
+SOCIAL_AUTH_B2ACCESS_KEY = getenv('SOCIAL_AUTH_B2ACCESS_KEY')
+SOCIAL_AUTH_B2ACCESS_SECRET = getenv('SOCIAL_AUTH_B2ACCESS_SECRET')
