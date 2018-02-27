de514725acd30e8efba5c8df2bc1ef40f2a35067
Hanne Moa <hanne.moa@uninett.no>
2018-02-27 14:52:18 +0100

    Create notesform regardless

--- src/easydmp/plan/views.py
+++ src/easydmp/plan/views.py
@@ -288,11 +288,9 @@ class NewQuestionView(AbstractQuestionView):
         return super().get_context_data(**kwargs)
 
     def get_notesform(self, **_):
-        form = None
         form_kwargs = self.get_form_kwargs()
         question = self.get_question()
-        if question.has_notes:
-            form = NotesForm(**form_kwargs)
+        form = NotesForm(**form_kwargs)
         return form
 
     def get_form(self, **_):
