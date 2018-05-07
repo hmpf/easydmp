cd330d3b37e103e3a8ce6556437706ef5001fc86
Hanne Moa <hanne.moa@uninett.no>
2018-05-07 09:40:26 +0200

    Move utils to separate file

--- src/easydmp/dmpt/models.py
+++ src/easydmp/dmpt/models.py
@@ -22,6 +22,7 @@ from django.utils.text import slugify
 from flow.graphviz import _prep_dotsource, view_dotsource, render_dotsource_to_file
 
 from .errors import TemplateDesignError
+from .utils import *
 from easydmp.utils import pprint_list
 
 """
@@ -50,77 +51,6 @@ INPUT_TYPES = (
     'multinamedurlonetext',
     'multidmptypedreasononetext',
 )
-
-DJANGO_TEMPLATE_ENGINE = engines['django']
-
-
-def render_from_string(template_string, context=None):
-    if context is None:
-        context = {}
-    template = DJANGO_TEMPLATE_ENGINE.from_string(template_string)
-    return template.render(context)
-
-
-def _force_items_to_str(dict_):
-    return {force_text(k): force_text(v) for k, v in dict_.items()}
-
-
-def print_url(urldict):
-    url = urldict['url']
-    name = urldict.get('name', '')
-    name = name if name else url
-    format = '<a href="{}">{}</a>'
-    return format_html(format, url, escape(name))
-
-
-class DeletionMixin:
-    """Extend django's model deletion functionality"""
-
-    def collect(self, using=None, **kwargs):
-        """Collect objects related to self
-
-        Designed to be extended. Get the collector for self with super()::
-
-            collector = super().collect(using=using, **kwargs)
-
-        Add a queryset with::
-
-            collector.collect(queryset)
-
-        Add an instance with::
-
-            collector.collect([instance])
-
-        Finally, return the collector.
-        """
-        using = using or router.db_for_write(self.__class__, instance=self)
-        assert self._get_pk_val() is not None, (
-            "%s object can't be deleted because its %s attribute is set to None." %
-            (self._meta.object_name, self._meta.pk.attname)
-        )
-
-        collector = NestedObjects(using=using)
-        collector.collect([self], **kwargs)
-        return collector
-
-    @transaction.atomic
-    def delete(self, **kwargs):
-        collector = self.collect(**kwargs)
-        return collector.delete()
-    delete.alters_data = True
-
-
-class RenumberMixin:
-
-    def _renumber_positions(self, objects):
-        """Renumber positions so that eg. (1, 2, 7, 12) becomes (1, 2, 3, 4)
-
-        <objects> must already be sorted by position.
-        """
-        for i, obj in enumerate(objects, 1):
-            obj.position = i
-            obj.save()
-    _renumber_positions.alters_data = True
 
 
 class Template(DeletionMixin, RenumberMixin, models.Model):
---
+++ src/easydmp/dmpt/utils.py
@@ -0,0 +1,86 @@
+from django.contrib.admin.utils import NestedObjects
+from django.db import router
+from django.db import transaction
+from django.template import engines
+from django.utils.encoding import force_text
+from django.utils.html import format_html, escape
+
+
+DJANGO_TEMPLATE_ENGINE = engines['django']
+
+__all__ = (
+    'render_from_string',
+    'force_items_to_str',
+    'print_url',
+    'DeletionMixin',
+    'RenumberMixin',
+)
+
+
+def render_from_string(template_string, context=None):
+    if context is None:
+        context = {}
+    template = DJANGO_TEMPLATE_ENGINE.from_string(template_string)
+    return template.render(context)
+
+
+def force_items_to_str(dict_):
+    return {force_text(k): force_text(v) for k, v in dict_.items()}
+
+
+def print_url(urldict):
+    url = urldict['url']
+    name = urldict.get('name', '')
+    name = name if name else url
+    format = '<a href="{}">{}</a>'
+    return format_html(format, url, escape(name))
+
+
+class DeletionMixin:
+    """Extend django's model deletion functionality"""
+
+    def collect(self, using=None, **kwargs):
+        """Collect objects related to self
+
+        Designed to be extended. Get the collector for self with super()::
+
+            collector = super().collect(using=using, **kwargs)
+
+        Add a queryset with::
+
+            collector.collect(queryset)
+
+        Add an instance with::
+
+            collector.collect([instance])
+
+        Finally, return the collector.
+        """
+        using = using or router.db_for_write(self.__class__, instance=self)
+        assert self._get_pk_val() is not None, (
+            "%s object can't be deleted because its %s attribute is set to None." %
+            (self._meta.object_name, self._meta.pk.attname)
+        )
+
+        collector = NestedObjects(using=using)
+        collector.collect([self], **kwargs)
+        return collector
+
+    @transaction.atomic
+    def delete(self, **kwargs):
+        collector = self.collect(**kwargs)
+        return collector.delete()
+    delete.alters_data = True
+
+
+class RenumberMixin:
+
+    def _renumber_positions(self, objects):
+        """Renumber positions so that eg. (1, 2, 7, 12) becomes (1, 2, 3, 4)
+
+        <objects> must already be sorted by position.
+        """
+        for i, obj in enumerate(objects, 1):
+            obj.position = i
+            obj.save()
+    _renumber_positions.alters_data = True
