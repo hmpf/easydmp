711e00feb294b33068a4d88d955f231585f98fde
Hanne Moa <hanne.moa@uninett.no>
2018-05-23 14:28:38 +0200

    Obfuscate email addresses in API

--- src/easydmp/auth/api/views.py
+++ src/easydmp/auth/api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework import serializers
 
-
 from easydmp.auth.models import User
+
+
+def truncate_email(email):
+    userpart, domain = email.rsplit('@', 1)
+    domain = str(len(domain))
+    return '{}@{}'.format(userpart, domain)
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,15 +15,27 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         view_name='user-detail',
         lookup_field='pk'
     )
+    truncated_email = serializers.SerializerMethodField()
+    obfuscated_username = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = [
             'id',
             'url',
-            'username',
-            'email',
+            'obfuscated_username',
+            'truncated_email',
         ]
+
+    def get_obfuscated_username(self, obj):
+        if not '@' in obj.username:
+            return obj.username
+        return truncate_email(obj.username)
+
+    def get_truncated_email(self, obj):
+        if '@' not in obj.email:
+            return ''
+        return truncate_email(obj.email)
 
 
 class UserViewSet(ReadOnlyModelViewSet):
