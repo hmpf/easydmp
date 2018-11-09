ce3454bd88953bca0059784901ae7e985821bbf5
Hanne Moa <hanne.moa@uninett.no>
2018-11-09 11:39:25 +0100

    Mark new questions as obligatory by default

--- src/easydmp/dmpt/models.py
+++ src/easydmp/dmpt/models.py
@@ -669,7 +669,7 @@ class Question(DeletionMixin, RenumberMixin, models.Model):
     help_text = models.TextField(blank=True)
     framing_text = models.TextField(blank=True)
     comment = models.TextField(blank=True, null=True)
-    obligatory = models.NullBooleanField(blank=True, null=True)
+    obligatory = models.NullBooleanField(blank=True, null=True, default=True)
     node = models.OneToOneField('flow.Node', related_name='payload',
                                 blank=True, null=True,
                                 on_delete=models.SET_NULL)
