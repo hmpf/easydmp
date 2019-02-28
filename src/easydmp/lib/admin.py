e5ef9fdb5368f0806ed5eac29540e6f48283aac2
Hanne Moa <hanne.moa@uninett.no>
2019-02-28 13:08:22 +0100

    Move common admin-functionality to new home

---
+++ src/easydmp/lib/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+
+class FakeBooleanFilter(admin.SimpleListFilter):
+
+    def lookups(self, request, _model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        lookup = '{}__isnull'.format(self.parameter_name)
+        if self.value() == 'yes':
+            return queryset.filter(**{lookup: False})
+        if self.value() == 'no':
+            return queryset.filter(**{lookup: True})
+
+
+class PublishedFilter(FakeBooleanFilter):
+    title = 'public'
+    parameter_name = 'published'
