a1491c7772e7af7fe3e706be99a7589367dc2609
Hanne Moa <hanne.moa@uninett.no>
2019-03-07 14:47:59 +0100

    Cache the graphs on docker startup

--- compose-dev-run.sh
+++ compose-dev-run.sh
@@ -4,4 +4,5 @@ export SECRET_KEY=$SECRET_KEY
 
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
+python3 manage.py cache_graphs -F -A
 gunicorn --log-level debug --access-logfile - easydmp.site.wsgi -b 0.0.0.0:8000
