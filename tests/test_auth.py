8f245c9fdd322d61cbda92c48b0ebbf640166069
Hanne Moa <hanne.moa@uninett.no>
2019-04-10 12:57:28 +0200

    Thoroughly test auth middleware

---
+++ tests/test_auth.py
@@ -0,0 +1,42 @@
+from unittest.mock import Mock
+
+from django import test
+from django.http import HttpResponseRedirect
+from django.test.client import RequestFactory
+
+from easydmp.auth.middleware import LoginRequiredMiddleware
+
+
+class TestLoginRequiredMiddleware(test.TestCase):
+    def setUp(self):
+        request = RequestFactory().get('/foo')
+        request.login_url = '/login'
+        request.user = Mock()
+        request.get_full_path = Mock(return_value='/bar')
+        self.request = request
+
+    def test_process_view_login_required_false(self):
+        view_func = lambda : None
+        view_func.login_required = False
+        result = LoginRequiredMiddleware().process_view(self.request, view_func, None, {})
+        self.assertIsNone(result)
+
+    @test.override_settings(PUBLIC_URLS=('/foo',))
+    def test_process_view_public_urls(self):
+        view_func = lambda : None
+        result = LoginRequiredMiddleware().process_view(self.request, view_func, None, {})
+        self.assertIsNone(result)
+
+    def test_process_view_authenticated(self):
+        view_func = lambda : None
+        self.request.user.is_authenticated = Mock(return_value=True)
+        result = LoginRequiredMiddleware().process_view(self.request, view_func, None, {})
+        self.assertIsNone(result)
+        delattr(self.request.user, 'is_authenticated')
+
+    def test_process_view_redirect(self):
+        view_func = lambda : None
+        self.request.user.is_authenticated = Mock(return_value=False)
+        result = LoginRequiredMiddleware().process_view(self.request, view_func, None, {})
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, HttpResponseRedirect)
