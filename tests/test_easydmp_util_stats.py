5796c01d1db76f44ad5d5b93175a555086aa2374
Hanne Moa <hanne.moa@uninett.no>
2019-12-10 13:53:58 +0100

    Fix stats-tests

--- tests/test_easydmp_util_stats.py
+++ tests/test_easydmp_util_stats.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.test import TestCase
+from django.utils.timezone import now as tznow
 
 from easydmp.auth.models import User
 from easydmp.dmpt.models import Template
@@ -8,15 +11,28 @@ from easydmp.utils.stats import stats
 
 class StatsTestCase(TestCase):
 
+    def setUp(self):
+        # django-guardian, *sigh*
+        AnonymousUser = User.objects.get()
+        self.min_all_user = 1
+        self.min_last_30days_user = 0
+        last_30days = tznow() - timedelta(days=30)
+        if AnonymousUser.date_joined >= last_30days:
+            self.min_last_30days_user = 1
+
     def test_no_users_no_stats(self):
         result = stats()
         expected = {
             'users': {
-                'total': 0,
-                'last_30days':0,
+                'all': self.min_all_user,
+                'last_30days': self.min_last_30days_user,
             },
             'plans': {
-                'total': 0,
+                'all': 0,
+                'last_30days':0,
+            },
+            'domains': {
+                'all': 0,
                 'last_30days':0,
             },
         }
@@ -24,22 +40,26 @@ class StatsTestCase(TestCase):
 
     def test_some_users_and_plans_some_stats(self):
         template = Template.objects.create(title='testtemplate')
-        u1 = User.objects.create(username='testuser1')
-        u2 = User.objects.create(username='testuser2')
-        Plan.objects.create(title='testplan1', added_by=u1, modified_by=u1,
+        u1 = User.objects.create(username='testuser1', email='a@b.com')
+        u2 = User.objects.create(username='testuser2', email='b@c.com')
+        Plan.objects.create(title='testplan1', joined_by=u1, modified_by=u1,
                             template=template)
-        Plan.objects.create(title='testplan2', added_by=u2, modified_by=u2,
+        Plan.objects.create(title='testplan2', joined_by=u2, modified_by=u2,
                             template=template)
 
         result = stats()
         expected = {
             'users': {
-                'total': 2,
-                'last_30days': 2,
+                'all': 2 + self.min_all_user,
+                'last_30days': self.min_last_30days_user,
             },
             'plans': {
-                'total': 2,
+                'all': 2,
                 'last_30days': 2,
+            },
+            'domains': {
+                'all': 2,
+                'last_30days':2,
             },
         }
         self.assertDictEqual(result, expected)
