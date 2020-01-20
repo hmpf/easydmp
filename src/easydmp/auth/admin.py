5e07fbd0eeaf4903167676c7a7c538f07673cc15
Hanne Moa <hanne.moa@uninett.no>
2020-01-20 13:15:55 +0100

    Add a full_name field to User
    This is for allowing correct exports to the RDA DMP Common Standard
    version 1, which insists on both full names and email addresses for any
    contact point/person.
    
    PSA attempts to get a field "fullname" for all backends, so we reuse
    that and map it to our user model.

--- src/easydmp/auth/admin.py
+++ src/easydmp/auth/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 from .models import User
 
@@ -7,3 +8,12 @@ from .models import User
 @admin.register(User)
 class EasyDMPUserAdmin(UserAdmin):
     date_hierarchy = 'date_joined'
+    list_display = ('username', 'email', 'full_name', 'is_staff')
+    search_fields = ('username', 'full_name', 'email')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('full_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups',)}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
