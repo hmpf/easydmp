30ec9af058bdd34abff2a31415ae381191fd5a18
Hanne Moa <hanne.moa@uninett.no>
2020-06-05 12:56:58 +0200

    Allow search on users in eventlog
    This search is heavy. "Actor" is so fra always a User, maybe change
    actor away from a GenericForeignKey to a ForeignKey?
    
    Furthermore, adding search for other relevant things will also be rather
    verbose and slow.

--- src/easydmp/eventlog/admin.py
+++ src/easydmp/eventlog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from django.contrib.auth import get_user_model
+
 from .models import EventLog
 
 
@@ -10,7 +12,9 @@ def false(*args, **kwargs):
 @admin.register(EventLog)
 class EventLogAdmin(admin.ModelAdmin):
     list_display = ['actor', 'verb', 'target', 'action_object', 'timestamp']
-    search_fields = ['actor', 'target', 'action_object']
+    # search_fields cannot contain generic foreign keys, and cannot be empty
+    # for search to work
+    search_fields = ['verb']
     list_filter = [
         'verb',
         ('actor_content_type', admin.RelatedOnlyFieldListFilter),
@@ -24,6 +28,25 @@ class EventLogAdmin(admin.ModelAdmin):
         fields = obj._meta.get_fields()
         fieldnames = [field.name for field in fields]
         return fieldnames
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        if '@' in search_term or search_term.startswith('user:'):  # user
+            user_ids = [str(uid) for uid in (get_user_model().objects
+                        .filter(username__contains=search_term)
+                        .values_list('id', flat=True))]
+            if not user_ids:
+                return queryset, use_distinct
+            content_type_id = 4
+            all = self.model.objects.all()
+            queryset |= all.filter(actor_object_id__in=user_ids,
+                                   actor_content_type_id=content_type_id)
+            queryset |= all.filter(target_object_id__in=user_ids,
+                                   target_content_type_id=content_type_id)
+            queryset |= all.filter(action_object_object_id__in=user_ids,
+                                   action_object_content_type_id=content_type_id)
+            use_distinct = True
+        return queryset, use_distinct
 
     # noop methods
     delete_model = false
