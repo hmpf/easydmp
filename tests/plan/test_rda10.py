f32520afbff596ea8b1f9765e1f2200590e427f5
Vegard B. Havdal <vbhavdal@gmail.com>
2020-10-15 12:51:34 +0200

    Remove comments

--- tests/plan/test_rda10.py
+++ tests/plan/test_rda10.py
@@ -19,12 +19,12 @@ class RdaTest(test.TestCase):
         self.assertEqual('eng', dmp['dmp']['language'])
         self.assertEqual('testplan1', dmp['dmp']['title'])
         self.assertEqual(plan_uuid, dmp['dmp']['dmp_id']['identifier'])
-        self.assertEqual('testuser1', dmp['dmp']['contact']['mbox'])  # TODO is this correct Hanne?
+        self.assertEqual('testuser1', dmp['dmp']['contact']['mbox'])
         self.assertEqual('testuser1', dmp['dmp']['contact']['name'])
         self.assertEqual(1, len(dmp['dmp']['dataset']))
         self.assertEqual('unknown', dmp['dmp']['dataset'][0]['personal_data'])
         self.assertEqual('unknown', dmp['dmp']['dataset'][0]['sensitive_data'])
         self.assertEqual('unknown', dmp['dmp']['ethical_issues_exist'])
         self.assertEqual(1, len(dmp['dmp']['contributor']))
-        self.assertEqual('testuser1', dmp['dmp']['contributor'][0]['mbox'])  # TODO Hanne intentional
+        self.assertEqual('testuser1', dmp['dmp']['contributor'][0]['mbox'])
         self.assertEqual('testuser1', dmp['dmp']['contributor'][0]['name'])
