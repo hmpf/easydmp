df4f72117a4b4de74074f8c6ccb747c112702480
Vegard B. Havdal <vbhavdal@gmail.com>
2020-12-09 13:20:24 +0100

    WIP date support

--- bin/pdfexport.py
+++ bin/pdfexport.py
@@ -1,3 +1,4 @@
+import datetime
 import re
 import sys
 import requests
@@ -6,20 +7,28 @@ import requests
 # Standalone script to batch download all plans from an EasyDMP instance
 #
 
-endpoint = sys.argv[0]
-token = sys.argv[1]
-dir = sys.argv[2]
+endpoint = sys.argv[1]
+token = sys.argv[2]
+dir = sys.argv[3]
+
+added_ts_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+from_date = datetime.datetime.strptime(sys.argv[4], added_ts_format) if len(sys.argv) >= 5 else None
+to_date = datetime.datetime.strptime(sys.argv[5], added_ts_format) if len(sys.argv) >= 6 else None
 
 auth_headers = {'Authorization': 'Bearer {}'.format(token)}
-r = requests.get('{}/api/v1/plans'.format(endpoint), headers=auth_headers)
+print('Getting plans from {}'.format(endpoint))
+if from_date:
+    print('From date: {}'.format(from_date))
+if to_date:
+    print('To date: {}'.format(to_date))
+r = requests.get('{}/api/v2/plans'.format(endpoint), headers=auth_headers)
 r.raise_for_status()
 plans = r.json()
+print('Saving {} plans to {}'.format(len(plans), dir))
 for plan in plans:
-    pdf_url = plan['generated_pdf_url']
+    pdf_url = '{}/api/v2/plans/{}/export.pdf'.format(endpoint, plan['id'])
     pr = requests.get(pdf_url, headers=auth_headers)
     pr.raise_for_status()
-    d = pr.headers['Content-Disposition']
-    fname = re.findall("filename=(.+)", d)[0]
-    file = open("{}{}".format(dir, fname), "w")
-    file.write(pr.content)
-    file.close()
+    cd = pr.headers['Content-Disposition']
+    with open("{}/{}".format(dir, re.findall('filename=(.+)', cd)[0]), 'wb') as f:
+        f.write(pr.content)
