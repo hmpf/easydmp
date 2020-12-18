3c622ec0981d1c3314a9bf8c8d88306ed5797063
Vegard B. Havdal <vbhavdal@gmail.com>
2020-12-18 12:48:43 +0100

    Expand testcase

--- tests/plan/models/test_AnswerSet.py
+++ tests/plan/models/test_AnswerSet.py
@@ -5,44 +5,73 @@ from easydmp.plan.models import Plan, AnswerSet, Answer
 from tests.dmpt.factories import TemplateFactory, SectionFactory
 
 
-class TestAnswerSet(test.TestCase):
+class TestAnswerSetValidation(test.TestCase):
 
-    def test_validate_answerset_data(self):
-        template = TemplateFactory()
-        plan = Plan(template=template, added_by_id=1, modified_by_id=1, valid=False)
-        plan.save()
-        section = SectionFactory.build(template=template)
-        section.save()
-        q1 = ShortFreetextQuestion(section=section, position=1)
-        q2 = BooleanQuestion(section=section, position=2)
-        q3 = PositiveIntegerQuestion(section=section, position=3)
-        q1.save()
-        q2.save()
-        q3.save()
+    def setUp(self):
+        self.template = TemplateFactory()
+        self.plan = Plan(template=self.template, added_by_id=1, modified_by_id=1, valid=False)
+        self.plan.save()
+        self.section = SectionFactory.build(template=self.template)
+        self.section.save()
+        self.q1 = ShortFreetextQuestion(section=self.section, position=1)
+        self.q2 = BooleanQuestion(section=self.section, position=2)
+        self.q3 = PositiveIntegerQuestion(section=self.section, position=3)
+        self.q1.save()
+        self.q2.save()
+        self.q3.save()
 
-        as1 = AnswerSet(plan=plan, section=section, valid=False, data={
-            str(q1.id): {
+    def test_validate_answerset_data_ok(self):
+        as1 = AnswerSet(plan=self.plan, section=self.section, valid=False, data={
+            str(self.q1.id): {
                 "choice": "foo",
                 "notes": "n1"
             },
-            str(q2.id): {
-                "choice": "True",
+            str(self.q2.id): {
+                "choice": "Yes",
                 "notes": "n2"
             },
-            str(q3.id): {
+            str(self.q3.id): {
                 "choice": 1,
                 "notes": "n3"
             }
         })
         as1.save()
-        a1_1 = Answer(answerset=as1, question=q1, plan=plan, valid=False)
-        a1_2 = Answer(answerset=as1, question=q2, plan=plan, valid=False)
-        a1_3 = Answer(answerset=as1, question=q3, plan=plan, valid=False)
-        a1_1.save()
-        a1_2.save()
-        a1_3.save()
-
+        a1, a2, a3 = self._add_answers(as1)
         as1.validate()
-        self.assertTrue(a1_1.valid)
-        self.assertTrue(a1_2.valid)
-        self.assertFalse(a1_3.valid)
+        self.assertTrue(Answer.objects.get(pk=a1.pk).valid)
+        self.assertTrue(Answer.objects.get(pk=a2.pk).valid)
+        self.assertTrue(Answer.objects.get(pk=a3.pk).valid)
+        self.assertTrue(as1.valid)
+
+    def _add_answers(self, as1):
+        a1 = Answer(answerset=as1, question=self.q1, plan=self.plan, valid=False)
+        a2 = Answer(answerset=as1, question=self.q2, plan=self.plan, valid=False)
+        a3 = Answer(answerset=as1, question=self.q3, plan=self.plan, valid=False)
+        a1.save()
+        a2.save()
+        a3.save()
+        return a1, a2, a3
+
+    def test_validate_answerset_data_fail(self):
+        as1 = AnswerSet(plan=self.plan, section=self.section, valid=False, data={
+            str(self.q1.id): {
+                "choice": None,
+                "notes": "n1"
+            },
+            str(self.q2.id): {
+                "choice": None,
+                "notes": "n2"
+            },
+            str(self.q3.id): {
+                "choice": "bogus",
+                "notes": "n3"
+            }
+        })
+        as1.save()
+        a1, a2, a3 = self._add_answers(as1)
+        as1.validate()
+        self.assertFalse(Answer.objects.get(pk=a1.pk).valid)
+        self.assertFalse(Answer.objects.get(pk=a2.pk).valid)
+        # TODO Hanne how is this valid answer for a PositiveIntegerQuestion:
+        self.assertTrue(Answer.objects.get(pk=a3.pk).valid)
+        self.assertFalse(as1.valid)
