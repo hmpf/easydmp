d613128b202847b7e080a7ea7a6f1968a43785ed
Vegard B. Havdal <vbhavdal@gmail.com>
2021-01-28 09:43:47 +0100

    Support --allplans

--- src/easydmp/lib/upgrade/management/commands/move_answer_data_to_answerset.py
+++ src/easydmp/lib/upgrade/management/commands/move_answer_data_to_answerset.py
@@ -26,15 +26,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('-p', '--plan', nargs='*', type=int, default=[],
-                            help='Plans to process (id). Default is all plans.')
+                            help='Plans to process (id). Default is no plans. Overrides --allplans if set.')
+        parser.add_argument('-a', '--allplans', type=bool, default=False,
+                            help='Whether to process all plans. Default is false')
         parser.add_argument('-d', '--delete', type=bool, default=False,
                             help='Whether to delete the answer data from Plan after it has been written to AnswerSet. Default is false. The command is idempotent only if this is false.')
 
     def handle(self, *args, **options):
         plan_ids = options['plan']
+        do_all_plans = options['allplans']
+        plans = []
         if plan_ids:
             plans = Plan.objects.filter(pk__in=plan_ids)
-        else:
+        elif do_all_plans:
             plans = Plan.objects.all()
         for plan in plans:
             handle_plan(plan, options['delete'])
