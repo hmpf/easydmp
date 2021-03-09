ce5890e959f83829be86e764d154657027052d1e
Hanne Moa <hanne.moa@uninett.no>
2021-03-09 07:48:08 +0100

    Add more tests of importing templates

--- tests/dmpt/test_import_template.py
+++ tests/dmpt/test_import_template.py
@@ -1,5 +1,8 @@
+import json
 from unittest import TestCase as UnitTestCase
+
 from django.test import TestCase as DjangoTestCase
+from django.test import tag
 
 from easydmp.dmpt.import_template import _prep_model_dict, _check_missing_input_types, TemplateImportError, _check_missing_eestore_types_and_sources, deserialize_template_export
 from easydmp.dmpt.models import INPUT_TYPES
@@ -58,3 +61,113 @@ class TestCheckMissingEEStoreTypesAndSources(DjangoTestCase):
         }]
         result = _check_missing_eestore_types_and_sources(eestore_mounts)
         self.assertTrue(result is None)
+
+
+@tag('unittest')
+class TestDeserilizeTemplateExport(UnitTestCase):
+
+    def test_garbage_should_fail(self):
+        with self.assertRaises(TemplateImportError) as e:
+            deserialize_template_export('vbgfnhj')
+            self.assertEqual(str(e), 'Template export is not JSON')
+
+    def test_empty_should_fail(self):
+        with self.assertRaises(TemplateImportError) as e:
+            deserialize_template_export('{}')
+            self.assertEqual(str(e), 'Template export is empty')
+
+    def test_malformed_should_fail(self):
+        with self.assertRaises(TemplateImportError) as e:
+            deserialize_template_export('{"a": 1}')
+            self.assertEqual(str(e), 'Template export is malformed')
+
+    def test_valid_should_succeed(self):
+        data = {
+            "comment": "",
+            "easydmp": {
+              "version": "1.5.0",
+              "origin": "examples",
+              "input_types": [
+                "bool",
+                "choice",
+                "date",
+                "daterange",
+                "extchoicenotlisted",
+                "externalchoice",
+                "externalmultichoiceonetext",
+                "extmultichoicenotlistedonetext",
+                "multichoiceonetext",
+                "multidmptypedreasononetext",
+                "multinamedurlonetext",
+                "multirdacostonetext",
+                "namedurl",
+                "positiveinteger",
+                "reason",
+                "shortfreetext",
+                "storageforecast"
+              ]
+            },
+            "template": {
+              "id": 64,
+              "modified": "2021-02-25T08:35:00.817527Z",
+              "cloned_from": None,
+              "cloned_when": None,
+              "title": "Section examples",
+              "abbreviation": "",
+              "description": "",
+              "more_info": "",
+              "domain_specific": False,
+              "reveal_questions": False,
+              "version": 1,
+              "created": "2021-02-25T08:35:00.817581Z",
+              "published": None,
+              "retired": None,
+              "input_types_in_use": [
+                "reason"
+              ]
+            },
+            "sections": [
+              {
+                "id": 184,
+                "modified": "2021-02-25T08:35:25.391259Z",
+                "cloned_from": None,
+                "cloned_when": None,
+                "template": 64,
+                "label": "",
+                "title": "A section, required",
+                "position": 1,
+                "introductory_text": "",
+                "comment": "",
+                "super_section": None,
+                "section_depth": 1,
+                "branching": False,
+                "optional": False,
+                "repeatable": False
+              },
+            ],
+            "questions": [
+                              {
+                "id": 625,
+                "cloned_from": None,
+                "cloned_when": None,
+                "input_type": "reason",
+                "section": 184,
+                "position": 1,
+                "label": "",
+                "question": "A q req",
+                "help_text": "",
+                "framing_text": "",
+                "comment": "",
+                "on_trunk": True,
+                "optional": False,
+                "optional_canned_text": ""
+              },
+            ],
+            "explicit_branches": [],
+            "canned_answers": [],
+            "eestore_mounts": [],
+        }
+        jsonblob = json.dumps(data)
+        result = deserialize_template_export(jsonblob)
+        self.assertEqual(data, result)
+
