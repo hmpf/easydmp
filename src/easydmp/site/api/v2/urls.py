cca2342c8fff967e9d725bb0289c4cef2676086e
Hanne Moa <hanne.moa@uninett.no>
2021-05-11 10:21:40 +0200

    Get rid of unused and broken jwt endpoint

--- src/easydmp/site/api/v2/urls.py
+++ src/easydmp/site/api/v2/urls.py
@@ -5,7 +5,6 @@ from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 
 from easydmp.auth.api.v2 import router as auth_router
-from easydmp.auth.api.v2.views import authorize_jwt_token
 from easydmp.dmpt.api.v2 import router as dmpt_router
 from easydmp.lib.api.routers import ContainerRouter
 from easydmp.plan.api.v2 import router as plan_router
@@ -15,7 +14,6 @@ jwt_urls = [
     path('jwt/authenticate/', obtain_jwt_token, name='obtain_jwt_token',),
     path('jwt/refresh/', refresh_jwt_token, name='refresh_jwt_token'),
     path('jwt/verify/', verify_jwt_token, name='verify_jwt_token'),
-    path('jwt/authorize/', authorize_jwt_token, name='authorize_jwt_token'),
 ]
 
 router = ContainerRouter()
